8c8b84d20ce04c00333ddf5cd45124ef3f4e187b
lucas-developer1 <lucasbernhardt0@gmail.com>
2025-12-20 19:42:12 +0100

    Create utils.js

---
+++ scripts/utils.js
@@ -0,0 +1,8 @@
+/**
+ * Leinenchallenge - Gemeinsame Funktionen
+ * Helper-Funktionen die von beiden Quiz-Seiten genutzt werden
+ */
+
+console.log('Utils Script loaded - Version 1.0.0');
+
+// Gemeinsame Funktionen, API-Calls, etc.
