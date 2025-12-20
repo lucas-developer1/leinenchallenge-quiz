a78ace16d30e881c3dae7237b5e64063114ab548
lucas-developer1 <lucasbernhardt0@gmail.com>
2025-12-20 19:39:52 +0100

    Create quiz-main.js

---
+++ scripts/quiz-main.js
@@ -0,0 +1,8 @@
+/**
+ * Leinenchallenge - Haupt Quiz (30 Schritte)
+ * Ersetzt InputFlow mit finish_flow System
+ */
+
+console.log('Quiz Main Script loaded - Version 1.0.0');
+
+// Hier kommt dein Haupt-Quiz Code rein
